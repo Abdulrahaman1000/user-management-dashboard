7da1292d761805673e4295c98d82f1a14f366553
Abdulrahman Adekeye <58373414+Abdulrahaman1000@users.noreply.github.com>
2025-05-11 15:56:24 -0700

    baseURL

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
-  withCredentials: true,
+  baseURL: 'https://user-management-dashboard-1-v4xj.onrender.com/api',
+  withCredentials: true, // Only needed if your backend uses cookies for auth
 });
 
 export default api;
